2df84d3928c0f025d4eb40ddc03ec291faa8ddf7
badurka1 <dawidbadura111@gmail.com>
2020-05-28 18:32:19 +0200

    Aplikacja

---
+++ FoodDelivery12/app/src/main/java/com/example/fooddelivery/ui/home/HomeViewModel.java
@@ -0,0 +1,24 @@
+package com.example.fooddelivery.ui.home;
+
+import android.widget.ImageView;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Gotowanie to świetna recepta na monotonię. Szczególnie wtedy, jeśli zamiast powielać znane, będziemy wypróbowywać nowe przepisy. Gdzie ich szukać? Oczywiście u nas! Znajdziesz tu zarówno proste receptury w sam raz dla tych, którzy stawiają pierwsze kroki w kuchni, jak i bardziej złożone przepisy dla doświadczonych kucharzy.");
+
+
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
